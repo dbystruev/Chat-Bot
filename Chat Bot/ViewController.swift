e15c94ca38340d070e309590807ffd342a762c09
Denis Aleksandrovich Bystruev <dbystruev@me.com>
2019-02-04 16:21:03 +0300

    Added outlets and actions

--- Chat Bot/ViewController.swift
+++ Chat Bot/ViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var answerLabel: UILabel!
+    @IBOutlet weak var questionTextView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+    }
 }
 
