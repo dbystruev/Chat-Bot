488dcc7d89a4d1a1e0cfb33131c13d18dbe8dbc5
Denis Bystruev <dbystruev@me.com>
2019-02-25 14:42:48 +0300

    Added answers.plist with the answers

---
+++ Chat Bot/Answerer.swift
@@ -0,0 +1,13 @@
+//
+//  Answerer.swift
+//  Chat Bot
+//
+//  Created by Denis Bystruev on 25/02/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+struct Answerer {
+    
+    
+    
+}
